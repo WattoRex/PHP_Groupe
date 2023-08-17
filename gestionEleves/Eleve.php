c619753667ba39b6b57a79fb09041cbccc23fe65
WattoRex <39130413+WattoRex@users.noreply.github.com>
2023-08-17 09:10:02 +0200

    Gestion Ficiher

--- gestionEleves/Eleve.php
+++ gestionEleves/Eleve.php
@@ -115,4 +115,50 @@ class Eleve
 
         return $this;
     }
+
+
+}
+
+function saisirDonneesEleve(): Eleve
+{
+    while (true) {
+        $nom = readline("Entrez le nom de l'élève : ");
+
+        // Supprimer les espaces éventuels avant et après la saisie
+        $nom = trim($nom);
+
+        if (!empty($nom) && preg_match('/^[a-zA-Z]+$/', $nom)) {
+            break; // Sortir de la boucle si le nom est valide
+        } else {
+            echo "Veuillez entrer un nom valide (contenant uniquement des lettres et non vide)." . PHP_EOL;
+        }
+    }
+    $listeNotes = [];
+
+    while (true) {
+        $note = readline("Entrez une note (ou 'q' pour quitter) : ");
+
+        if ($note === 'q') {
+            break; // Sortir de la boucle si l'utilisateur entre 'q'
+        }
+
+        $note = trim($note); // Supprimer les espaces éventuels avant et après la saisie
+
+        if ($note !== '' && is_numeric($note)) {
+            $note = (int) $note;
+
+            if ($note >= 0 && $note <= 20) {
+                $listeNotes[] = $note; // Ajouter la note à la liste si elle est valide
+            } else {
+                echo "La note doit être comprise entre 0 et 20." . PHP_EOL;
+            }
+        } else {
+            echo "Veuillez entrer une note valide (nombre entre 0 et 20)." . PHP_EOL;
+        }
+    }
+
+    $eleve = new Eleve($nom, $listeNotes, null, 0); // Créer un objet Eleve avec les données saisies
+    $eleve->getMoyenne(); // Calculer la moyenne de l'élève 
+    $eleve->setMoyenne(null); // Définir la moyenne 
+    return $eleve; // Retourner l'objet Eleve créé
 }
--- mainWithCSV.php
+++ mainWithCSV.php
@@ -1,51 +1,6 @@
 <?php
 include_once("./gestionEleves/Eleve.php"); // Inclure la classe Eleve depuis le chemin spécifié
 
-// Fonction pour saisir les données d'un élève
-function saisirDonneesEleve(): Eleve
-{
-    while (true) {
-        $nom = readline("Entrez le nom de l'élève : ");
-
-        // Supprimer les espaces éventuels avant et après la saisie
-        $nom = trim($nom);
-
-        if (!empty($nom) && preg_match('/^[a-zA-Z]+$/', $nom)) {
-            break; // Sortir de la boucle si le nom est valide
-        } else {
-            echo "Veuillez entrer un nom valide (contenant uniquement des lettres et non vide)." . PHP_EOL;
-        }
-    }
-    $listeNotes = [];
-
-    while (true) {
-        $note = readline("Entrez une note (ou 'q' pour quitter) : ");
-
-        if ($note === 'q') {
-            break; // Sortir de la boucle si l'utilisateur entre 'q'
-        }
-
-        $note = trim($note); // Supprimer les espaces éventuels avant et après la saisie
-
-        if ($note !== '' && is_numeric($note)) {
-            $note = (int) $note;
-
-            if ($note >= 0 && $note <= 20) {
-                $listeNotes[] = $note; // Ajouter la note à la liste si elle est valide
-            } else {
-                echo "La note doit être comprise entre 0 et 20." . PHP_EOL;
-            }
-        } else {
-            echo "Veuillez entrer une note valide (nombre entre 0 et 20)." . PHP_EOL;
-        }
-    }
-
-    $eleve = new Eleve($nom, $listeNotes, null, 0); // Créer un objet Eleve avec les données saisies
-    $eleve->getMoyenne(); // Calculer la moyenne de l'élève 
-    $eleve->setMoyenne(null); // Définir la moyenne 
-    return $eleve; // Retourner l'objet Eleve créé
-}
-
 $elevesData = []; // Tableau pour stocker les objets Eleve saisis
 
 while (true) {
@@ -65,8 +20,13 @@ while (true) {
 
 // Écrire les données des élèves dans un fichier CSV
 $csvFileName = "Eleve.csv";
-$csvFile = fopen($csvFileName, 'w');
-fputcsv($csvFile, array_keys($elevesData[0])); // Écrire l'en-tête du CSV
+$csvFile = fopen($csvFileName, 'a');
+
+// Vérifier si le fichier est vide (nouveau fichier)
+$fileIsEmpty = filesize($csvFileName) === 0;
+if ($fileIsEmpty) {
+    fputcsv($csvFile, array_keys($elevesData[0])); // Écrire l'en-tête du CSV
+}
 
 foreach ($elevesData as $eleveData) {
     fputcsv($csvFile, $eleveData); // Écrire les données de chaque élève dans le CSV
